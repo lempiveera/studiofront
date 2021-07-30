ff9f7771f25d0b2538f099c8fb5d82c127747db6
lempiveera <bgm092@myy.haaga-helia.fi>
2021-07-30 03:40:25 +0300

    Appbar and Tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import Presentlist from './Components/Presentlist';
-import React from 'react';
-
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+//import Typography from '@material-ui/core/Typography';
 
 function App() {
 
+  const [value, setValue] = useState('one');
+
+  const handleChange = (event, value) => {
+    setValue(value);
+  }
 
   return (
     <div className="App">
-        <Presentlist />
+      <AppBar position="static" color="transparent"> 
+        <Tabs textColor="inherit" indicatorColor="primary" value={value} onChange={handleChange}>
+          <Tab value="one" label="Current situation"/>
+          <Tab value="two" label="Downstairs" />
+          <Tab value="three" label="Upstrais" />
+          <Tab value="four" label="Sumu" />
+        </Tabs>
+      </AppBar>
+        {value === 'one' && <Presentlist />}
+        {value === 'two' && <Presentlist />}
+        {value === 'three' && <Presentlist />}
+        {value === 'four' && <Presentlist />}
     </div>
   );
 }
